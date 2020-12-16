386ab83213002d795eddca8ac036917b2261ef52
Sarah-Elhelw <sarah_elhelw@hotmail.fr>
2020-12-16 16:53:23 +0100

    Trying to improve insert method

--- src/main/java/io/github/GuedidiElHelw/BTrees/BNode.java
+++ src/main/java/io/github/GuedidiElHelw/BTrees/BNode.java
@@ -1,129 +1,116 @@
 package io.github.GuedidiElHelw.BTrees;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 public class BNode {
 	private int m;
-	private int n;	// Number of keys
+	private int n; // Number of keys
 	private ArrayList<Integer> keys;
-    private ArrayList<BNode> bNodes;
-    private BNode parent;
-    private boolean isLeaf;
- 
-    public BNode(int m, boolean leaf) {
-        this.m = m;
-        this.n = 0;
-        this.keys = new ArrayList<>();
-        this.bNodes = new ArrayList<>();
-        this.parent = null;
-        this.isLeaf = leaf;
-    }
-    
-    public BNode(int m, int n, ArrayList<Integer> keys, ArrayList<BNode> bNodes, )
- 
-    public void printBTree() {
-        int i = 0;
-        for (i = 0; i < this.n; i++) {
-            if (this.isLeaf == false) {
-                bNodes.get(i).printBTree();
-            }
-            System.out.print(keys.get(i) + " ");
-        }
-        if (isLeaf == false)
-            bNodes.get(i).printBTree();
-    }
- 
-    
-    class IsFoundBNode{
+	private ArrayList<BNode> bNodes;
+	private BNode parent;
+	private boolean isLeaf;
+
+	public BNode(int m, boolean leaf) {
+		this.m = m;
+		this.n = 0;
+		this.keys = new ArrayList<>();
+		this.bNodes = new ArrayList<>();
+		this.parent = null;
+		this.isLeaf = leaf;
+	}
+
+	public void printBTree() {
+		int i = 0;
+		for (i = 0; i < this.n; i++) {
+			if (this.isLeaf == false) {
+				bNodes.get(i).printBTree();
+			}
+			System.out.print(keys.get(i) + " ");
+		}
+		if (isLeaf == false)
+			bNodes.get(i).printBTree();
+	}
+
+	class IsFoundBNode {
 		public boolean isFound;
 		public BNode bn;
-		
-		public IsFoundBNode(BNode bn, boolean isFound){
-			this.isFound = isFound ;
+
+		public IsFoundBNode(BNode bn, boolean isFound) {
+			this.isFound = isFound;
 			this.bn = bn;
 		}
 	}
-    
-    public IsFoundBNode search(int k) {
- 
-        int i = 0;
-        while (i < n && k > keys.get(i)) {
-        	i++;
-        }
- 
-        if (keys.get(i) == k)
-            return new IsFoundBNode(this, true);
- 
-        if (isLeaf == true)
-            return new IsFoundBNode(this, false);
- 
-        return bNodes.get(i).search(k);
- 
-    }
-    
-    public void split(int k) {
-    	
-    	keys.add(k);
-    	Collections.sort(keys);
-    	
-    	// Second half :
-    	List<Integer> temp = keys.subList(m/2+1, keys.size());
-    	
-    	// Insertion in parent :
-    	int medianK = keys.remove(m/2);
-    	parent.keys.add(medianK);
-    	Collections.sort(parent.keys);
-    	parent.bNodes.add(parent.keys.indexOf(medianK)+1, new BNode(temp, m);
-    	
-    	// First half :
-    	keys.removeAll(temp);
-    	
-    }
-    
-    public ArrayList<BNode> getbNodes() {
-		return bNodes;
+
+	public IsFoundBNode search(int k) {
+
+		int i = 0;
+		while (i < n && k > keys.get(i)) {
+			i++;
+		}
+
+		if (keys.get(i) == k)
+			return new IsFoundBNode(this, true);
+
+		if (isLeaf == true)
+			return new IsFoundBNode(this, false);
+
+		return bNodes.get(i).search(k);
+
 	}
-    
-    public ArrayList<Integer> getKeys() {
+
+	public boolean insert(int k) {
+		IsFoundBNode ifbn = search(k);
+
+		// Case 1 : k is found in the tree
+		if (ifbn.isFound == true) {
+			return false;
+		}
+
+		BNode bn = ifbn.bn; // leaf where k should be inserted if not found
+		int n = bn.getN();
+		ArrayList<Integer> keys = bn.getKeys();
+
+		// Case 2 : it is possible to insert the key directly
+		if (n < m - 1) {
+			keys.add(k);
+			Collections.sort(keys);
+			return true;
+		}
+
+		// Case 3 : A split is needed
+		bn.split(k);
+		return true;
+	}
+
+	public void split(int k) {
+
+		keys.add(k);
+		Collections.sort(keys);
+
+		// Second half :
+		ArrayList<Integer> temp = new ArrayList<>(keys.subList(m / 2 + 1, keys.size()));
+
+		// New node
+		BNode newBNode = new BNode(m, isLeaf);
+		newBNode.keys = temp;
+
+		// Insertion in parent :
+		int medianK = keys.remove(m / 2);
+		parent.insert(medianK);
+		parent.bNodes.add(newBNode);
+
+		// First half :
+		keys.removeAll(temp);
+
+	}
+
+	public ArrayList<Integer> getKeys() {
 		return keys;
 	}
-    
-    public int getM() {
-		return m;
-	}
-    
-    public int getN() {
+
+	public int getN() {
 		return n;
 	}
-    
-    public BNode getParent() {
-		return parent;
-	}
-    
-    public void setbNodes(ArrayList<BNode> bNodes) {
-		this.bNodes = bNodes;
-	}
-    
-    public void setKeys(ArrayList<Integer> keys) {
-		this.keys = keys;
-	}
-    
-    public void setLeaf(boolean isLeaf) {
-		this.isLeaf = isLeaf;
-	}
-    
-    public void setM(int m) {
-		this.m = m;
-	}
-    
-    public void setN(int n) {
-		this.n = n;
-	}
-    
-    public void setParent(BNode parent) {
-		this.parent = parent;
-	}
+
 }
--- src/main/java/io/github/GuedidiElHelw/BTrees/BTree.java
+++ src/main/java/io/github/GuedidiElHelw/BTrees/BTree.java
@@ -1,70 +1,43 @@
 package io.github.GuedidiElHelw.BTrees;
 
-import java.util.Arrays;
-
 import io.github.GuedidiElHelw.BTrees.BNode.IsFoundBNode;
 
 public class BTree {
 	private int m; // the order of the tree
 	private BNode root;
- 
-    public BTree(int m) {
-    	this.m = m;
-    	root = null;
-    }
- 
-    public void printBTree() {
-        if (root != null)
-            root.printBTree();
-        System.out.println();
-    }
- 
-    public IsFoundBNode search(int k) {
-        if (root == null)
-            return null;
-        else
-            return root.search(k);
-    }
-    
-    public boolean insert(int k) {	// return false is k is in the tree
-    	
-    	// Case 1 : there is no root
-    	if (root == null) {
-    		root = new BNode(m, true);
-    	}
-    	
-    	IsFoundBNode ifbn = root.search(k);
-    	
-    	// Case 2 : k is found in the tree
-    	if (ifbn.isFound == true) {
-    		return false;
-    	}
-    	
-    	BNode bn = ifbn.bn;	// node where k should be inserted if not found
-    	int n = bn.getN();
-    	int[] keys = bn.getKeys();
-    	
-    	
-    	// Case 3 : it is possible to insert the key directly
-    	if (n < m-1) {
-    		keys[n] = k;
-    		n++;
-    		Arrays.sort(keys);
-    		return true;
-    	}
-    	
-    	// Case 4 : A split is needed
-    	bn.split(k);
-    	
-    	
-    }
-    
-    public int getM() {
+
+	public BTree(int m) {
+		this.m = m;
+		root = null;
+	}
+
+	public void printBTree() {
+		if (root != null)
+			root.printBTree();
+		System.out.println();
+	}
+
+	public IsFoundBNode search(int k) {
+		if (root == null)
+			return null;
+		else
+			return root.search(k);
+	}
+
+	public boolean insert(int k) { // return false if k is in the tree
+		if (root == null) {
+			root = new BNode(m, true);
+		}
+
+		return root.insert(k);
+	}
+
+	public int getM() {
 		return m;
 	}
-    
-    public BNode getRoot() {
+
+	public BNode getRoot() {
 		return root;
 	}
-    
+
 }
